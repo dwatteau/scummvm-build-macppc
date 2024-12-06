1ba7ac3e0564a492a49a9f46bce144079bd549ee
Donovan Watteau <contrib@dwatteau.fr>
2024-12-06 15:12:24 +0100

    MACOSXPPC: Update libjpeg-turbo to a GCC 7.5.0 build as well, and restore -O3 -DNDEBUG from upstream
    I'm usually not a fan of -O3 on non-mainstream architectures (and
    especially for old unmaintained libraries), but I trust the
    libjpeg-turbo upstream for this.

--- toolchains/macosx-ppc/packages/libjpeg-turbo/build.sh
+++ toolchains/macosx-ppc/packages/libjpeg-turbo/build.sh
@@ -11,7 +11,7 @@ do_make_bdir
 
 do_http_fetch libjpeg-turbo "https://downloads.sourceforge.net/libjpeg-turbo/libjpeg-turbo-${JPEGTURBO_VERSION}.tar.gz" 'tar xzf' #"sha256:${JPEGTURBO_SHA256}"
 
-do_cmake -DENABLE_SHARED=0 -DWITH_TURBOJPEG=0 -DREQUIRE_SIMD=1 -DCMAKE_C_FLAGS="-Wa,-force_cpusubtype_ALL" -DCMAKE_C_COMPILER=/opt/macports-tff/bin/gcc-mp-4.8 -DCMAKE_OSX_DEPLOYMENT_TARGET=10.4 -DCMAKE_OSX_SYSROOT=/Developer/SDKs/MacOSX10.4u.sdk
+do_cmake -DENABLE_SHARED=0 -DWITH_TURBOJPEG=0 -DREQUIRE_SIMD=1 -DCMAKE_C_FLAGS="-O3 -DNDEBUG -mmacosx-version-min=10.4 -isysroot /Developer/SDKs/MacOSX10.4u.sdk -Wa,-force_cpusubtype_ALL -m32" -DCMAKE_C_COMPILER=/opt/macports-tff/bin/gcc-mp-7 -DCMAKE_OSX_DEPLOYMENT_TARGET=10.4 -DCMAKE_OSX_SYSROOT=/Developer/SDKs/MacOSX10.4u.sdk
 
 do_make VERBOSE=1
 
