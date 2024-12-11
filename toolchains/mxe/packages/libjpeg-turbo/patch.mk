2bc37ef5ccb2027d8bacd9532121f131d604dd45
Donovan Watteau <contrib@dwatteau.fr>
2024-12-11 01:04:18 +0100

    Remove old dockerized-bb infrastructure that's no longer truly used

--- toolchains/mxe/packages/libjpeg-turbo/patch.mk
+++
@@ -1,5 +0,0 @@
-# Patch libjpeg-turbo_BUILD to install libjpeg-turbo in the correct place in the prefix
-# newline variable is defined by MXE
-$(eval define libjpeg-turbo_BUILD$(newline)\
-  $(subst /$$(PKG),,$(value libjpeg-turbo_BUILD))$(newline)\
-endef)
--- toolchains/riscos/functions-platform.sh
+++
@@ -1,9 +0,0 @@
-do_svn_fetch () {
-	if [ -d "$1"*/ ]; then
-		rm -rf "$1"*/
-	fi
-	svn co "$2" "$1"
-	cd "$1"*/
-	svn up $3
-	do_patch
-}
