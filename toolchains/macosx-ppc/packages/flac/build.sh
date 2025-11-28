6ba3ea105c686a28700a4e7ea43e15edc56c5707
Donovan Watteau <contrib@dwatteau.fr>
2025-11-28 08:08:10 +0100

    MACOSXPPC: Make sure that the FLAC build doesn't use C11 features
    Like it's already done for some other modern configure scripts which
    tend to do this nowadays.

--- toolchains/macosx-ppc/packages/flac/build.sh
+++ toolchains/macosx-ppc/packages/flac/build.sh
@@ -19,6 +19,7 @@ export SDKROOT=/Developer/SDKs/MacOSX10.4u.sdk
 CC=/opt/macports-tff/bin/gcc-mp-7 \
 CFLAGS='-O2 -mmacosx-version-min=10.4 -isysroot /Developer/SDKs/MacOSX10.4u.sdk -Wa,-force_cpusubtype_ALL -m32' \
 LDFLAGS='-Wl,-macosx_version_min,10.4 -Wl,-syslibroot,/Developer/SDKs/MacOSX10.4u.sdk' \
+ac_cv_prog_cc_c11=no \
 do_configure \
 --disable-avx \
 --disable-stack-smash-protection --disable-rpath \
