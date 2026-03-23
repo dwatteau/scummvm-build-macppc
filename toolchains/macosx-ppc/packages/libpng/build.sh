9a9526f1761229a888b30de89f665fcc8b4bc64e
Donovan Watteau <contrib@dwatteau.fr>
2026-03-23 22:26:29 +0100

    MACOSXPPC: Update libpng to 1.6.55

--- toolchains/macosx-ppc/packages/libpng/build.sh
+++ toolchains/macosx-ppc/packages/libpng/build.sh
@@ -1,8 +1,6 @@
 #! /bin/sh
 
-# XXX: currently a bit newer than what Debian 13 provides, as for some reason
-# some CVEs haven't been backported yet, there?
-LIBPNG_VERSION=1.6.52
+LIBPNG_VERSION=1.6.55
 #LIBPNG_SHA256=af4fb7f260f839919e5958e5ab01a275d4fe436d45442a36ee62f73e5beb75ba
 
 PACKAGE_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
